0be568d5c159c49bda04d02951b3b03951f3bccc
root <Ruthosammor>
2022-03-22 21:00:16 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,14 +8,7 @@
 */
 
 void reset_to_98(int *n)
-<<<<<<< HEAD
-{	;	*n = 98;
-=======
-{
+{	
 	*n = 98;
-<<<<<<< HEAD
->>>>>>> f4869be9c995f34d6e6bec0e07999598b2edbd62
-}		
-=======
+
 }
->>>>>>> ff20da3c60080a5fbffbbfb0cb7e9aba02391c5e
